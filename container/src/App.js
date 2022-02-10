9504758ebfe08cea754046fef6a0ba5bc50af13e
Dmytro Poliuliuk <dpoli@ciklum.com>
2022-02-10 22:15:59 +0200

    integration of the Container and Marketing

--- container/src/App.js
+++ container/src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { mount as marketingMount } from "marketing/MarketingApp";
+
+// console.log(marketingMount);
 
 export default () => {
   return <h1>Container</h1>;
